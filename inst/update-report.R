5b37da94a6c7a2cf65c3126803aeea445308a2a5
jtrecenti <julio.trecenti@gmail.com>
2022-04-16 20:14:24 -0300

    essa ideia é muito ruim

--- inst/update-report.R
+++
@@ -1,6 +0,0 @@
-rmarkdown::render_site("inst/book")
-pagedown::chrome_print(
-  "inst/book/obs_test.html",
-  output = "inst/book/relatorio/obs_test.pdf",
-  extra_args = c("--disable-gpu", "--no-sandbox")
-)
